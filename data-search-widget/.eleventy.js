775b147e9cb9e6e470b53751aa51a0d91df6b98b
Elvi Shu <elvishu@users.noreply.github.com>
2025-05-21 15:38:10 +1000

    [QOLOE-986] Add location search with distance radius filter
    Add the following on the main data-search-widget:
    - location based search, recognising suburb or postcode search
    - filter location search with the distance within X km radius
    - updated the error message to handle various error, eg: API connectivity issue
    - converting filterItems function to async to wait for API response
    
    Add helpers for:
    - async await function for API call to get lat/lon of a location from Open Data Locality datasource
    - distance calculation using Haversine formula
    
    Add a few example html files for local preview
    
    Add support for JSON data on eleventy and an example file

--- data-search-widget/.eleventy.js
+++ data-search-widget/.eleventy.js
@@ -3,7 +3,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css/data-search-widget.css")
   eleventyConfig.addPassthroughCopy("css/layout.css")
   eleventyConfig.addPassthroughCopy("data-search-widget.js")
+  eleventyConfig.addPassthroughCopy("utils/helpers.js")
   eleventyConfig.addPassthroughCopy("**/*.csv");
+  eleventyConfig.addPassthroughCopy("examples/*.json");
   eleventyConfig.addPassthroughCopy("./node_modules/bootstrap/dist/css/bootstrap.min.css")
   eleventyConfig.addPassthroughCopy("./node_modules/bootstrap/dist/js/bootstrap.min.js")
   eleventyConfig.addPassthroughCopy("./node_modules/jquery/dist/jquery.min.js")
@@ -13,7 +15,8 @@ module.exports = function (eleventyConfig) {
     watch: [
       "_site/**/*.css",
       "_site/**/*.js",
-      "_site/**/*.csv"
+      "_site/**/*.csv",
+      "_site/**/*.json"
     ]
   })
 }
