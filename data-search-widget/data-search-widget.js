6dec3e14e65d06249e77bb0985f76b4a295aaba1
Ghazal Salehi <ghazal.salehi@smartservice.qld.gov.au>
2025-08-25 11:37:35 +1000

    Checking if map object is not null

--- data-search-widget/data-search-widget.js
+++ data-search-widget/data-search-widget.js
@@ -917,7 +917,7 @@
             if (results.length) {
               searchTool.template.paginate(results)
             } else {
-              if (config.maps) {
+              if (config.maps && map) {
                 map.fitBounds(qldMapBounds);
               }
               $('.page-summary, .pager').hide();
